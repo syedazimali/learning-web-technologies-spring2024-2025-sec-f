b0e65eb74338ac769f7a0282030b809f8ef15dad
syedazimali <azim200233@gmail.com>
2025-04-28 14:52:33 +0600

    Add files via upload

---
+++ L01.php
@@ -0,0 +1,15 @@
+<?php
+
+$length = 10;
+$width = 5;
+
+$area = $length*$width;
+
+$perimeter =2*( $length*$width;)
+
+echo "Length :$length<br>";
+echo "width :$width<br>";
+echo "area of rechtangle :$area<br>";
+echo "perimeter :$perimeter<br>";
+
+?>
